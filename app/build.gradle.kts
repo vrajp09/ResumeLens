e40025d516abe44ef41f6d51153f424da5c4bbeb
mrama <rmahesh172003@gmail.com>
2025-11-05 17:01:17 -0600

    feat: add Resume Analysis screen UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.runtime.saveable)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
