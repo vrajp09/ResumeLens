5ca566f14edf1b2d2daef1e41d72b295357245cb
Vraj Patel <vraj.p009@gmail.com>
2025-11-14 16:15:24 -0600

    inital commit to firestore branch

--- app/src/main/java/com/cs407/resumelens/auth/AuthViewModel.kt
+++ app/src/main/java/com/cs407/resumelens/auth/AuthViewModel.kt
@@ -10,6 +10,7 @@ data class AuthUiState(
     val loading: Boolean = false,
     val error: String? = null,
     val isSignedIn: Boolean = Firebase.auth.currentUser != null
+
 )
 
 class AuthViewModel(
