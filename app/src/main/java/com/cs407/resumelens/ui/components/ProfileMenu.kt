b79aed7188e9f46513d7a775eebb9b0e2600c5aa
Vraj Patel <vraj.p009@gmail.com>
2025-11-14 14:48:15 -0600

    commit of gradle file, and profile menu

--- app/src/main/java/com/cs407/resumelens/ui/components/ProfileMenu.kt
+++ app/src/main/java/com/cs407/resumelens/ui/components/ProfileMenu.kt
@@ -3,7 +3,14 @@ package com.cs407.resumelens.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
@@ -17,6 +24,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.android.car.ui.toolbar.MenuItem
 import com.cs407.resumelens.R
 
 @Composable
